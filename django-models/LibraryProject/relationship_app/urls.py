f12371e3558fd0889e4fab8cf066e32e15962e0e
Powei Jeffrey Erewejoh <79942537+Epowei@users.noreply.github.com>
2025-02-23 20:16:15 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('member/', views.member_view, name='member_view'),
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
