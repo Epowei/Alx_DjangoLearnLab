313f8b1f5d46b662ed692b9c8267208cb490e6c2
Epowei <erejef@yahoo.com>
2025-02-27 10:55:39 +0100

    added advanced features folder

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Author, Book, Library, Librarian, UserProfile
 
 class BookAdmin(admin.ModelAdmin):
     # Display these fields in the list view
@@ -11,5 +13,33 @@ class BookAdmin(admin.ModelAdmin):
     # Enable search by title and author
     search_fields = ('title', 'author')
 
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'role']
+    list_filter = ['date_of_birth']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    def role(self, obj):
+        return obj.userprofile.role if hasattr(obj, 'userprofile') else 'N/A'
+    role.short_description = 'Role'
+
 # Register the Book model with the custom admin class
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
+admin.site.register(UserProfile)
