980288dcb2c313105fdcf256a0307f525633e8c6
Epowei <erejef@yahoo.com>
2025-03-29 09:58:05 +0100

    modified

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    bio = models.TextFieldField(max_length=255, blank=True)
+    bio = models.TextField(max_length=255, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from .models import CustomUser
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,14 +18,15 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = CustomUser(
+        # Use get_user_model() to dynamically get the custom user model
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        # Explicitly create a token for the new user
+        token = Token.objects.create(user=user)
         return user
     
 class LoginSerializer(serializers.Serializer):
